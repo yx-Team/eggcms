e1379fea0e69a2783462890837b49cfcf94dbc8a
w530385371 <w530385371@163.com>
2018-10-09 18:15:05 +0800

    导入数据

---
+++ cmsdb/导入数据.bat
@@ -0,0 +1,3 @@
+mongoimport -d cmsdb -c role --file D:/other/egg-learning/eggcms/cmsdb/role.json
+mongoimport -d cmsdb -c access --file D:/other/egg-learning/eggcms/cmsdb/access.json
+mongoimport -d cmsdb -c admin --file D:/other/egg-learning/eggcms/cmsdb/admin.json
