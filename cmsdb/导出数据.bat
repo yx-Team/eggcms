7ddd1261b518bf06d0a45973dc1b224c9b8ad719
w530385371 <w530385371@163.com>
2018-10-09 17:55:43 +0800

    导出数据

---
+++ cmsdb/导出数据.bat
@@ -0,0 +1,3 @@
+mongoexport -d cmsdb -c role -o D:/other/egg-learning/eggcms/cmsdb/role.json
+mongoexport -d cmsdb -c access -o D:/other/egg-learning/eggcms/cmsdb/access.json
+mongoexport -d cmsdb -c admin -o D:/other/egg-learning/eggcms/cmsdb/admin.json
