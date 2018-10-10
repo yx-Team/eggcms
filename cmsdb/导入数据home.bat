27ed83cf2cc7a2974791a55beecdd069cf0fdd69
w530385371 <w530385371@163.com>
2018-10-10 18:20:08 +0800

    home数据

--- cmsdb/导入数据home.bat
+++ cmsdb/导入数据home.bat
@@ -1,3 +1,4 @@
 mongoimport -d cmsdb -c role --file D:/work/egg/eggcms/cmsdb/role.json
 mongoimport -d cmsdb -c access --file D:/work/egg/eggcms/cmsdb/access.json
 mongoimport -d cmsdb -c admin --file D:/work/egg/eggcms/cmsdb/admin.json
+mongoimport -d cmsdb -c role_access --file D:/work/egg/eggcms/cmsdb/role_access.json
--- cmsdb/导出数据home.bat
+++ cmsdb/导出数据home.bat
@@ -1,3 +1,4 @@
 mongoexport -d cmsdb -c role -o D:/work/egg/eggcms/cmsdb/role.json
 mongoexport -d cmsdb -c access -o D:/work/egg/eggcms/cmsdb/access.json
 mongoexport -d cmsdb -c admin -o D:/work/egg/eggcms/cmsdb/admin.json
+mongoexport -d cmsdb -c role_access -o D:/work/egg/eggcms/cmsdb/role_access.json
